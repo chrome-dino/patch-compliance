efefddc757754fe9157e30381341f41f65c56131
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-06-09 02:55:02 -0500

    Update __main__.py

--- src/patch_compliance/__main__.py
+++ src/patch_compliance/__main__.py
@@ -15,7 +15,7 @@ def main():
     for x in os_whitelist:
         valid += "'" + x + "',"
     valid = valid[:-1]
-    parser.add_argument("-f", "--file", help="Name of the host file containing line seperated list of IPs or FQDNs. Defaults to hosts.txt",required=False)
+    parser.add_argument("-f", "--host_file", help="Name of the host file containing line seperated list of IPs or FQDNs. Defaults to hosts.txt",required=False)
     parser.add_argument("-o", "--operating_systems", help="Comma seperated list of operating systems. Valid OSs are as follows: " + valid,required=True)
 
 
@@ -26,7 +26,7 @@ def main():
     os_list = args.operating_systems.split(',')
     os_final = []
     
-    # catch in valid OS entries
+    # catch invalid OS entries
     for x in os_list:
         if x.strip() not in os_whitelist:
             print(x.strip() + ' is an invalid OS. Valid OSs are as follows:')
@@ -35,8 +35,8 @@ def main():
             exit(-1)
         else:
             os_final.append(x.strip())
-    
-    audit = patch_compliance.PatchCompliance(host_file=args.file, os_list=os_final)
+
+    audit = patch_compliance.PatchCompliance(os_list=os_final,host_file=args.host_file)
     audit.run()
     
 if __name__ == "__main__":
